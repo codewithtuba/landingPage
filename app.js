05f2eb53b11b5b362cff8f253f13297060036531
codewithtuba <101013919+codewithtuba@users.noreply.github.com>
2023-06-09 21:15:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,18 @@
+const menu = document.querySelector('.menu');
+const menuBtn = document.querySelector('.menu-btn');
+
+//    Toggle open/close menu
+
+menuBtn.addEventListener('click', () => {
+    menu.classList.toggle('menu-open');
+});
+
+//////////////////////////////////////
+
+alert("Welcome to My Website...");
+
+
+
+
+
+
